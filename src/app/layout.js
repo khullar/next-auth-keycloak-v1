f452a64e3bc5f5d094793c2f4208dd6070968e84
Gaurav K <161233+khullar@users.noreply.github.com>
2025-06-05 22:22:19 +0530

    Basic auth with Keycloak is implemented

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
+import { SessionProvider } from "next-auth/react";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -19,10 +15,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} antialiased`}>
+        <SessionProvider>{children}</SessionProvider>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,26 @@
+"use client";
+import { useSession } from "next-auth/react";
+
+import SignIn from "@/components/sign-in";
+import SignOut from "@/components/sign-out";
+
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        Hello World
+        {session ? (
+          <>
+            <p>Welcome, {session.user.name}!</p>
+            <SignOut />
+          </>
+        ) : (
+          <>
+            <p>You are not logged in.</p>
+            <SignIn />
+          </>
+        )}
       </main>
     </div>
   );
